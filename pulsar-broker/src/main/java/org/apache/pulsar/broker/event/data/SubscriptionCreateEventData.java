a34dcd2a65f5e0ae4b6127b07b0e931eebd451dc
Zixuan Liu <nodeces@gmail.com>
2025-08-08 11:24:19 +0800

    [fix][broker] Add constructor for event data
    Signed-off-by: Zixuan Liu <nodeces@gmail.com>

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/event/data/SubscriptionCreateEventData.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/event/data/SubscriptionCreateEventData.java
@@ -18,7 +18,9 @@
  */
 package org.apache.pulsar.broker.event.data;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.NoArgsConstructor;
 import lombok.Value;
 import org.apache.pulsar.broker.service.TopicEventsListener.EventData;
 import org.apache.pulsar.client.api.MessageId;
@@ -26,6 +28,8 @@ import org.apache.pulsar.common.api.proto.CommandSubscribe.InitialPosition;
 
 @Builder
 @Value
+@NoArgsConstructor(force = true)
+@AllArgsConstructor
 public class SubscriptionCreateEventData implements EventData {
     String subscriptionName;
     boolean durable;
