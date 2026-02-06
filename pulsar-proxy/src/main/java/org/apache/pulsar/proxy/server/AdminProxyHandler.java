16bcfc9e79cdcea559c3d370b0ba71aa3e2d143f
Zixuan Liu <nodeces@gmail.com>
2026-02-06 23:02:58 +0800

    [improve][broker] Add idle timeout support for http (#25224)
    (cherry picked from commit 63220eadcfc11f43d4537327a3593603ee5be697)

--- pulsar-proxy/src/main/java/org/apache/pulsar/proxy/server/AdminProxyHandler.java
+++ pulsar-proxy/src/main/java/org/apache/pulsar/proxy/server/AdminProxyHandler.java
@@ -113,6 +113,8 @@ class AdminProxyHandler extends ProxyServlet {
             protocolHandlers.put(new RedirectProtocolHandler(httpClient));
         }
 
+        httpClient.setIdleTimeout(config.getHttpProxyIdleTimeout());
+
         setTimeout(config.getHttpProxyTimeout());
     }
 
